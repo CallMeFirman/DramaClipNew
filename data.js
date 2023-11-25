b7f38d025a3a2d87404389e771a379d15769c729
CallMeFirman <firmananjelo5@gmail.com>
2023-11-25 14:29:14 +0700

    [Bab 5] Navigation

--- data.js
+++ data.js
@@ -1,101 +1,101 @@
-export const CategoryList = [
-    {
-    id: 1,
-    categoryName: 'Popular',
-    },
-    {
-    id: 2,
-    categoryName: 'Genre',
-    },
-    {
-    id: 3,
-    categoryName: 'Tahun',
-    },
-    {
-    id: 4,
-    categoryName: 'Recommend',
-    },
-    {
-    id: 5,
-    categoryName: 'Ftv',
-    },
-    {
-    id: 6,
-    categoryName: 'Horor',
-    },
-    ];
-    export const BlogList = [
-        {
-          id: 1,
-          title: 'Moving',
-          category: 'Drama Korea',
-          image:
-            'https://radarlampung.disway.id/upload/41d276020812ec6463db3b40d263414a.jpeg',
-          createdAt: 'Nov 10, 2023',
-        },
-        {
-          id: 2,
-          title: 'Kokdu season of deity',
-          category: 'Drama Korea',
-          image:
-          'https://images.herzindagi.info/image/2022/Dec/korean-dramas-releasing-in-january-23.jpg',
-          createdAt: 'Nov 10, 2023',
-        },
-        {
-          id: 3,
-          title: 'Si Ganteng & Si Cantik',
-          category: 'Drama China',
-          image:
-          'https://cdns.klimg.com/kapanlagi.com/p/mv5bmtk2mdnjnzetzjuyzc00nzjjlwi3zjatmje2nddmmzg5yzljxkeyxkfqcgdeqxvymti4ntc5odc5._v1_fmjpg_ux1000_.jpg',
-          createdAt: 'Nov 10, 2023',
-        },
-        {
-          id: 4,
-          title: 'Swadikap',
-          category: 'Drama Thailand',
-          image:
-          'https://dianiopiari.com/wp-content/uploads/2020/08/dxgW5_4f.jpg',
-          createdAt: 'Nov 10, 2023',
-        },
-        {
-          id: 5,
-          title: 'Goblin',
-          category: 'Drama Korea',
-          image:
-          'https://media.suara.com/pictures/970x544/2022/06/01/70852-drama-korea-tentang-cinta-beda-dunia-hancinema.jpg',
-          createdAt: 'Nov 10, 2023',
-          totalComments: 89,
-        },
-        {
-          id: 6,
-          title: 'Percintaan 2 Manusia',
-          category: 'Drama China',
-          image:
-            'https://cdn0-production-images-kly.akamaized.net/mUDNThRWP_H71VShBARA6PDOJsc=/1200x1200/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/4442556/original/030335600_1685093339-Crush.jpg',
-          createdAt: 'Nov 5, 2023',
-        },
-        {
-          id: 7,
-          title: 'Descendant Of The Sun',
-          category: 'Drama Korea',
-          image:
-            'https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2022/09/04/3656970508.jpg',
-          createdAt: 'Nov 5, 2023',
-        },
-        {
-          id: 8,
-          title: 'Percintaan 2 muda - mudi',
-          category: 'Drama Thailand',
-          image:
-            'https://live.firstmedia.com/files/images/article/2022/02/Suka%20Drama%20Romantis,%20Ini%203%20Rekomendasi%20Drama%20Thailand%20Untukmu.jpg',
-          createdAt: 'Nov 5, 2023',
-        },
-        {
-          id: 9,
-          title: 'Mengejar Surga',
-          category: 'Drama Indonesia',
-          image:
-            'https://o-cdn-cas.sirclocdn.com/parenting/images/Mengejar_Surga.width-800.format-webp.webp',
-          createdAt: 'Nov 5, 2023',
-        },
-      ];    
+export const BlogList = [
+  {
+  id: 1,
+  title: 'Moving',
+  category: 'Drama Korea',
+  image:
+  'https://radarlampung.disway.id/upload/41d276020812ec6463db3b40d263414a.jpeg',
+  createdAt: 'Nov 10, 2023',
+  totalComments: 50,
+  totalLikes: 1500,
+  content: 'Moving adalah drama aksi superhero yang diadaptasi dari webtoon populer dengan judul yang sama karya Kang Full. Berkisah tentang Kim Doo Shik (Jo In Sung), agen veteran yang memiliki kekuatan superhero untuk terbang.'
+  },
+  {
+  id: 2,
+  title: 'Kokdu season of deity',
+  category: 'Drama Korea',
+  image:
+  'https://images.herzindagi.info/image/2022/Dec/korean-dramas-releasing-in-january-23.jpg',
+  createdAt: 'Nov 10, 2023',
+  totalComments: 60,
+  totalLikes: 2000,
+  content: 'Drama ini berkisah tentang percintaan malaikat maut dengan seorang dokter. Mengusung genre fantasi romantis, tokoh dokter yang bernama Han Gye-jeol (Im Soo-hyang) ini digambarkan sebagai manusia yang punya kemampuan khusus.'
+  },
+  {
+  id: 3,
+  title: 'Nothing but you',
+  category: 'Drama China',
+  image:
+  'https://cdns.klimg.com/kapanlagi.com/p/mv5bmtk2mdnjnzetzjuyzc00nzjjlwi3zjatmje2nddmmzg5yzljxkeyxkfqcgdeqxvymti4ntc5odc5._v1_fmjpg_ux1000_.jpg',
+  createdAt: 'Nov 10, 2023',
+  totalComments: 45,
+  totalLikes: 450,
+  content: 'Serial Nothing But You mengisahkan seorang wanita karir yang sedang menjalani masa-masa sulit dalam hidupnya. Ia dilanda kekhawatiran terkait kemajuan karirnya hingga suatu hari   wanita tersebut bertemu dengan seorang atlet dengan karakter humoris yang dapat menghangatkan suasana.'
+  },
+  {
+  id: 4,
+  title: 'So Wayree',
+  category: 'Drama Thailand',
+  image:
+  'https://dianiopiari.com/wp-content/uploads/2020/08/dxgW5_4f.jpg',
+  createdAt: 'Nov 10, 2023',
+  totalComments: 70,
+  totalLikes: 3000,
+  content: 'So Wayree menceritakan tentang dua anak dari dua keluarga pengusaha di bidang real estate. Namanya Parin dan Paramita.'
+  },
+  {
+  id: 5,
+  title: 'Goblin',
+  category: 'Drama Korea',
+  image:
+  'https://media.suara.com/pictures/970x544/2022/06/01/70852-drama-korea-tentang-cinta-beda-dunia-hancinema.jpg',
+  createdAt: 'Nov 10, 2023',
+  totalComments: 89,
+  totalLikes: 10000,
+  content: 'serial Goblin menceritakan tentang Kim Shin, seorang jenderal perang yang hebat di zaman kuno. Ia dibunuh oleh raja muda yang cemburu pada kehebatannya. Kim Shin akhirnya menjadi Dokkaebi (Goblin), yang memiliki kehidupan abadi.'
+  },
+  {
+  id: 6,
+  title: 'Percintaan 2 Manusia',
+  category: 'Drama China',
+  image:
+  'https://cdn0-production-images-kly.akamaized.net/mUDNThRWP_H71VShBARA6PDOJsc=/1200x1200/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/4442556/original/030335600_1685093339-Crush.jpg',
+  createdAt: 'Nov 5, 2023',
+  totalComments: 60,
+  totalLikes: 1300,
+  content: 'Chinese drama CRUSH merupakan drama romantis dengan kisah yang menyentuh. Sehingga cocok untuk kalian yang selama ini suka nonton film dan drama romansa. Drama yang tayang pada 2021 ini, mengisahkan tentang cinta tulus yang menerima apa adanya.'
+  },
+  {
+  id: 7,
+  title: 'Descendant Of The Sun',
+  category: 'Drama Korea',
+  image:
+  'https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2022/09/04/3656970508.jpg',
+  createdAt: 'Nov 5, 2023',
+  totalComments: 300,
+  totalLikes: 50000,
+  content: 'menceritakan tentang kisah cinta serta perjuangan para tentara dan tenaga kesehatan di Urk. Sebenarnya, kisah mereka sudah dimulai sejak masih di Korea Selatan. Penugasan mereka ke sebuah daerah yang dipenuhi konflik menjadi jalan untuk semakin mengukuhkan hubungan.'
+  },
+  {
+  id: 8,
+  title: 'Sweet Sin',
+  category: 'Drama Thailand',
+  image:
+  'https://live.firstmedia.com/files/images/article/2022/02/Suka%20Drama%20Romantis,%20Ini%203%20Rekomendasi%20Drama%20Thailand%20Untukmu.jpg',
+  createdAt: 'Nov 5, 2023',
+  totalComments: 45,
+  totalLikes: 500,
+  content: 'Drama Thailand SWEET SIN bercerita tentang seorang pria kaya bernama Peat. Walaupun berasal dari keluarga kaya raya, faktanya Peat tak pernah bahagia karena harus hidup di bawah tekanan. Ibu Peat telah meninggal, dan dia punya hubungan yang kurang baik dengan sang ayah.'
+  },
+  {
+  id: 9,
+  title: 'Mengejar Surga',
+  category: 'Drama Indonesia',
+  image:
+  'https://o-cdn-cas.sirclocdn.com/parenting/images/Mengejar_Surga.width-800.format-webp.webp',
+  createdAt: 'Nov 5, 2023',
+  totalComments: 100,
+  totalLikes: 3000,
+  content: 'Mengejar Surga adalah film Islam drama religi produksi Viera Film yang disutradarai oleh Bambang Drias, serta dibintangi oleh Jessica Mila dan Al Ghazali.'
+  },
+  ];
